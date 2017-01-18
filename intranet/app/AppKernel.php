a5d412acd54275b1cac641b19bbd77c1fda155d5
GMarguiller <gael.marguiller@supinternet.fr>
2017-01-18 15:57:26 +0100

    create Entity and IntranetBundle

--- intranet/app/AppKernel.php
+++ intranet/app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new UserBundle\UserBundle()
+            new UserBundle\UserBundle(),
+            new IntranetBundle\IntranetBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
